7a2947ac05c405a9b8b1050db8710a60bcda134c
Rehan Fargose <74831072+RehanFargose@users.noreply.github.com>
2025-01-27 16:18:11 +0530

    Add files via upload
    C++ files with basic code

---
+++ basics.cpp
@@ -0,0 +1,37 @@
+// To take user input/give output use this lib
+#include <iostream>
+// To use getline import the string lib
+#include <string>
+using namespace std;
+
+int main(){
+    // // // School grading system task
+    // int marks = 69;
+    // cin >> marks;
+    // if (marks < 25 && marks >= 0){
+    //     cout << "F grade";
+    // }
+    // else if(marks <= 44){
+    //     cout << "E grade";
+    // }
+    // else if(marks <= 49){
+    //     cout << "D grade";
+    // }
+    // else if(marks <= 59){
+    //     cout << "C grade";
+    // }
+    // else if(marks <= 79){
+    //     cout << "B grade";
+    // }
+    // else if(marks <= 100){
+    //     cout << "A grade";
+    // }
+    // else{
+    //     cout << "Enter valid marks";
+    // }
+
+    cout << "STL viva experiment 2 Github: Updated the given code and made changes, now waiting to commit" << endl;
+    cout << "Making extra changes in new branch";
+
+    return 0;
+}
